cd30d981c2de34c027c2d0ad0b39be5d7bc4efa4
Jamshidbek <jamshidbek@example.com>
2026-05-20 23:42:38 +0500

    fix: remove whitenoise dependency, fix wsgi.py

--- startapp/settings.py
+++ startapp/settings.py
@@ -24,7 +24,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -32,8 +31,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'startapp.urls'
 
--- startapp/wsgi.py
+++ startapp/wsgi.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-# Loyiha yo'lagini qo'shamiz
 path = '/home/onlymetan/metanGaz'
 if path not in sys.path:
     sys.path.insert(0, path)
@@ -9,11 +8,4 @@ if path not in sys.path:
 os.environ['DJANGO_SETTINGS_MODULE'] = 'startapp.settings'
 
 from django.core.wsgi import get_wsgi_application
-django_app = get_wsgi_application()
-
-# WhiteNoise orqali static fayllarni to'g'ridan-to'g'ri serve qilish
-try:
-    from whitenoise import WhiteNoise
-    application = WhiteNoise(django_app, root='/home/onlymetan/metanGaz/static', prefix='static')
-except ImportError:
-    application = django_app
+application = get_wsgi_application()
